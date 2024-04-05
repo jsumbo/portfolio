9b23de94feec04ade0122abc32cfd5a94df9a089
Jallah <123503232+jsumbo@users.noreply.github.com>
2024-04-05 03:57:48 +0200

    Fix RSS feed in gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,7 +5,7 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
+    title: 'Jallah Sumbo',
     site_url: 'https://yoursite.com',
     feed_url: 'https://yoursite.com/feed.xml'
   })
